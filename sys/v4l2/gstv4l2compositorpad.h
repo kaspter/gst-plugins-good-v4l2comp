854186350cae13974f27298ae03fa621f8f53e01
Sebastien MATZ <sebastien.matz@veo-labs.com>
2016-06-02 17:03:17 +0200

    v4l2compositor: initial version
    Signed-off-by: Sebastien MATZ <sebastien.matz@veo-labs.com>

---
+++ sys/v4l2/gstv4l2compositorpad.h
@@ -0,0 +1,83 @@
+/* Video compositor plugin using V4L2 abilities
+ * Copyright (C) 2016 Sebastien MATZ <sebastien.matz@veo-labs.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin St, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ */
+
+#ifndef __GST_V4L2_COMPOSITOR_PAD_H__
+#define __GST_V4L2_COMPOSITOR_PAD_H__
+
+#include <gst/gst.h>
+#include <gst/video/video.h>
+
+G_BEGIN_DECLS
+#define GST_TYPE_V4L2_COMPOSITOR_PAD (gst_v4l2_compositor_pad_get_type())
+#define GST_V4L2_COMPOSITOR_PAD(obj) \
+        (G_TYPE_CHECK_INSTANCE_CAST((obj),GST_TYPE_V4L2_COMPOSITOR_PAD, GstV4l2CompositorPad))
+#define GST_V4L2_COMPOSITOR_PAD_CLASS(klass) \
+        (G_TYPE_CHECK_CLASS_CAST((klass),GST_TYPE_V4L2_COMPOSITOR_PAD, GstV4l2CompositorPadClass))
+#define GST_IS_V4L2_COMPOSITOR_PAD(obj) \
+        (G_TYPE_CHECK_INSTANCE_TYPE((obj),GST_TYPE_V4L2_COMPOSITOR_PAD))
+#define GST_IS_V4L2_COMPOSITOR_PAD_CLASS(klass) \
+        (G_TYPE_CHECK_CLASS_TYPE((klass),GST_TYPE_V4L2_COMPOSITOR_PAD))
+typedef struct _GstV4l2CompositorPad GstV4l2CompositorPad;
+typedef struct _GstV4l2CompositorPadClass GstV4l2CompositorPadClass;
+typedef struct _GstV4l2CompositorJob GstV4l2CompositorJob;
+
+struct _GstV4l2CompositorJob
+{
+  GstV4l2CompositorJob *master_job;
+  GstV4l2CompositorPad *cpad;
+  GstBuffer *external_sink_buf;
+  GstBuffer *sink_buf;
+  GstBuffer *source_buf;
+  gboolean pending;
+  gboolean queued;
+};
+
+/**
+ * GstV4l2CompositorPad:
+ *
+ * The opaque #GstV4l2CompositorPad structure.
+ */
+struct _GstV4l2CompositorPad
+{
+  GstV4l2VideoAggregatorPad parent;
+  GstV4l2M2m *m2m;
+  GList *pending_jobs;
+  GList *queued_jobs;
+
+  /* properties */
+  gint xpos, ypos;
+  gint width, height;
+  gchar *videodev;
+
+  GstBufferPool *peer_pool;
+
+  GstVideoConverter *convert;
+  GstVideoInfo conversion_info;
+  GstBuffer *converted_buffer;
+};
+
+struct _GstV4l2CompositorPadClass
+{
+  GstV4l2VideoAggregatorPadClass parent_class;
+};
+
+GType gst_v4l2_compositor_pad_get_type (void);
+
+G_END_DECLS
+#endif /* __GST_V4L2_COMPOSITOR_PAD_H__ */
